e2433a304d05dfb081553d45d0916542b566447c
benronan <benronan@users.noreply.github.com>
2018-09-13 16:07:04 -0500

    Update SinricSwitch.ino

--- SinricSwitch/SinricSwitch.ino
+++ SinricSwitch/SinricSwitch.ino
@@ -13,17 +13,21 @@ WiFiClient client;
 #define MySSID "xxxx" // TODO: Change to your Wifi network SSID
 #define MyWifiPassword "xxxxx" // TODO: Change to your Wifi network password
 
-#define API_ENDPOINT "http://sinric.com"
 #define HEARTBEAT_INTERVAL 300000 // 5 Minutes 
 
 uint64_t heartbeatTimestamp = 0;
 bool isConnected = false;
 
+void setPowerStateOnServer(String deviceId, String value);
+void setTargetTemperatureOnServer(String deviceId, String value, String scale);
+
+// deviceId is the ID assgined to your smart-home-device in sinric.com dashboard. Copy it from dashboard and paste it here
+
 void turnOn(String deviceId) {
   if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of first device
   {  
     Serial.print("Turn on device id: ");
-    Serial.println(deviceId);    
+    Serial.println(deviceId);
   } 
   else if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of second device
   { 
@@ -40,7 +44,7 @@ void turnOff(String deviceId) {
    if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of first device
    {  
      Serial.print("Turn off Device ID: ");
-     Serial.println(deviceId);     
+     Serial.println(deviceId);
    }
    else if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of second device
    { 
@@ -69,15 +73,12 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
         Serial.printf("[WSc] get text: %s\n", payload);
         // Example payloads
 
-        // For Light device type
+        // For Switch or Light device types
         // {"deviceId": xxxx, "action": "setPowerState", value: "ON"} // https://developer.amazon.com/docs/device-apis/alexa-powercontroller.html
-        // {"deviceId": xxxx, "action": "AdjustBrightness", value: 3} // https://developer.amazon.com/docs/device-apis/alexa-brightnesscontroller.html
-        // {"deviceId": xxxx, "action": "setBrightness", value: 42} // https://developer.amazon.com/docs/device-apis/alexa-brightnesscontroller.html
-        // {"deviceId": xxxx, "action": "SetColor", value: {"hue": 350.5,  "saturation": 0.7138, "brightness": 0.6501}} // https://developer.amazon.com/docs/device-apis/alexa-colorcontroller.html
-        // {"deviceId": xxxx, "action": "DecreaseColorTemperature"} // https://developer.amazon.com/docs/device-apis/alexa-colortemperaturecontroller.html
-        // {"deviceId": xxxx, "action": "IncreaseColorTemperature"} // https://developer.amazon.com/docs/device-apis/alexa-colortemperaturecontroller.html
-        // {"deviceId": xxxx, "action": "SetColorTemperature", value: 2200} // https://developer.amazon.com/docs/device-apis/alexa-colortemperaturecontroller.html
-        
+
+        // For Light device type
+        // Look at the light example in github
+          
         DynamicJsonBuffer jsonBuffer;
         JsonObject& json = jsonBuffer.parseObject((char*)payload); 
         String deviceId = json ["deviceId"];     
@@ -91,164 +92,10 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
                 turnOff(deviceId);
             }
         }
-        else if(action == "SetColor") {
-            // Alexa, set the device name to red
-            // get text: {"deviceId":"xxxx","action":"SetColor","value":{"hue":0,"saturation":1,"brightness":1}}
-            String hue = json ["value"]["hue"];
-            String saturation = json ["value"]["saturation"];
-            String brightness = json ["value"]["brightness"];
-
-            Serial.println("[WSc] hue: " + hue);
-            Serial.println("[WSc] saturation: " + saturation);
-            Serial.println("[WSc] brightness: " + brightness);
-        }
-        else if(action == "setBrightness") {
-          
-        }
-        else if(action == "AdjustBrightness") {
-          
-        }
-        else if (action == "test") {
-            Serial.println("[WSc] received test command from sinric.com");
-        }
-      }
-      break;
-    case WStype_BIN:
-      Serial.printf("[WSc] get binary length: %u\n", length);
-      break;
-  }
-}
-
-void setup() {
-  Serial.begin(115200);
-  
-  WiFiMulti.addAP(MySSID, MyWifiPassword);
-  Serial.println();
-  Serial.print("Connecting to Wifi: ");
-  Serial.println(MySSID);  
-
-  // Waiting for Wifi connect
-  while(WiFiMulti.run() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  if(WiFiMulti.run() == WL_CONNECTED) {
-    Serial.println("");
-    Serial.print("WiFi connected. ");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
-  }
-
-  // server address, port and URL
-  webSocket.begin("io
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <WebSocketsClient.h> //  https://github.com/kakopappa/sinric/wiki/How-to-add-dependency-libraries 
-#include <ArduinoJson.h> // https://github.com/kakopappa/sinric/wiki/How-to-add-dependency-libraries
-
-ESP8266WiFiMulti WiFiMulti;
-WebSocketsClient webSocket;
-WiFiClient client;
-
-#define MyApiKey "xxxxx" // TODO: Change to your sinric API Key. Your API Key is displayed on sinric.com dashboard
-#define MySSID "xxxx" // TODO: Change to your Wifi network SSID
-#define MyWifiPassword "xxxxx" // TODO: Change to your Wifi network password
-
-#define API_ENDPOINT "http://sinric.com"
-#define HEARTBEAT_INTERVAL 300000 // 5 Minutes 
-
-uint64_t heartbeatTimestamp = 0;
-bool isConnected = false;
-
-void turnOn(String deviceId) {
-  if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of first device
-  {  
-    Serial.print("Turn on device id: ");
-    Serial.println(deviceId);    
-  } 
-  else if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of second device
-  { 
-    Serial.print("Turn on device id: ");
-    Serial.println(deviceId);
-  }
-  else {
-    Serial.print("Turn on for unknown device id: ");
-    Serial.println(deviceId);    
-  }     
-}
-
-void turnOff(String deviceId) {
-   if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of first device
-   {  
-     Serial.print("Turn off Device ID: ");
-     Serial.println(deviceId);     
-   }
-   else if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of second device
-   { 
-     Serial.print("Turn off Device ID: ");
-     Serial.println(deviceId);
-  }
-  else {
-     Serial.print("Turn off for unknown device id: ");
-     Serial.println(deviceId);    
-  }
-}
-
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-  switch(type) {
-    case WStype_DISCONNECTED:
-      isConnected = false;    
-      Serial.printf("[WSc] Webservice disconnected from sinric.com!\n");
-      break;
-    case WStype_CONNECTED: {
-      isConnected = true;
-      Serial.printf("[WSc] Service connected to sinric.com at url: %s\n", payload);
-      Serial.printf("Waiting for commands from sinric.com ...\n");        
-      }
-      break;
-    case WStype_TEXT: {
-        Serial.printf("[WSc] get text: %s\n", payload);
-        // Example payloads
-
-        // For Light device type
-        // {"deviceId": xxxx, "action": "setPowerState", value: "ON"} // https://developer.amazon.com/docs/device-apis/alexa-powercontroller.html
-        // {"deviceId": xxxx, "action": "AdjustBrightness", value: 3} // https://developer.amazon.com/docs/device-apis/alexa-brightnesscontroller.html
-        // {"deviceId": xxxx, "action": "setBrightness", value: 42} // https://developer.amazon.com/docs/device-apis/alexa-brightnesscontroller.html
-        // {"deviceId": xxxx, "action": "SetColor", value: {"hue": 350.5,  "saturation": 0.7138, "brightness": 0.6501}} // https://developer.amazon.com/docs/device-apis/alexa-colorcontroller.html
-        // {"deviceId": xxxx, "action": "DecreaseColorTemperature"} // https://developer.amazon.com/docs/device-apis/alexa-colortemperaturecontroller.html
-        // {"deviceId": xxxx, "action": "IncreaseColorTemperature"} // https://developer.amazon.com/docs/device-apis/alexa-colortemperaturecontroller.html
-        // {"deviceId": xxxx, "action": "SetColorTemperature", value: 2200} // https://developer.amazon.com/docs/device-apis/alexa-colortemperaturecontroller.html
-        
-        DynamicJsonBuffer jsonBuffer;
-        JsonObject& json = jsonBuffer.parseObject((char*)payload); 
-        String deviceId = json ["deviceId"];     
-        String action = json ["action"];
-        
-        if(action == "setPowerState") { // Switch or Light
+        else if (action == "SetTargetTemperature") {
+            String deviceId = json ["deviceId"];     
+            String action = json ["action"];
             String value = json ["value"];
-            if(value == "ON") {
-                turnOn(deviceId);
-            } else {
-                turnOff(deviceId);
-            }
-        }
-        else if(action == "SetColor") {
-            // Alexa, set the device name to red
-            // get text: {"deviceId":"xxxx","action":"SetColor","value":{"hue":0,"saturation":1,"brightness":1}}
-            String hue = json ["value"]["hue"];
-            String saturation = json ["value"]["saturation"];
-            String brightness = json ["value"]["brightness"];
-
-            Serial.println("[WSc] hue: " + hue);
-            Serial.println("[WSc] saturation: " + saturation);
-            Serial.println("[WSc] brightness: " + brightness);
-        }
-        else if(action == "setBrightness") {
-          
-        }
-        else if(action == "AdjustBrightness") {
-          
         }
         else if (action == "test") {
             Serial.println("[WSc] received test command from sinric.com");
@@ -304,4 +151,37 @@ void loop() {
           webSocket.sendTXT("H");          
       }
   }   
-  }
+}
+
+// If you are going to use a push button to on/off the switch manually, use this function to update the status on the server
+// so it will reflect on Alexa app.
+// eg: setPowerStateOnServer("deviceid", "ON")
+void setPowerStateOnServer(String deviceId, String value) {
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["deviceId"] = deviceId;
+  root["action"] = "setPowerState";
+  root["value"] = value;
+  StreamString databuf;
+  root.printTo(databuf);
+  
+  webSocket.sendTXT(databuf);
+}
+
+//eg: setPowerStateOnServer("deviceid", "CELSIUS", "25.0")
+void setTargetTemperatureOnServer(String deviceId, String value, String scale) {
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["action"] = "SetTargetTemperature";
+  root["deviceId"] = deviceId;
+  
+  JsonObject& valueObj = root.createNestedObject("value");
+  JsonObject& targetSetpoint = valueObj.createNestedObject("targetSetpoint");
+  targetSetpoint["value"] = value;
+  targetSetpoint["scale"] = scale;
+   
+  StreamString databuf;
+  root.printTo(databuf);
+  
+  webSocket.sendTXT(databuf);
+}
